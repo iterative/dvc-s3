2aafc409bad96b8236b6e0e595e38ff103cb884b
Ruslan Kuprieiev <kupruser@gmail.com>
2021-12-02 11:12:41 +0200

    fill out tests

--- dvc_s3/tests/test_dvc.py
+++ dvc_s3/tests/test_dvc.py
@@ -10,45 +10,40 @@ from dvc.testing.test_workspace import (  # noqa, pylint: disable=unused-import
 
 
 @pytest.fixture
-def cloud_name():
-    return "s3"
-
-@pytest.fixture
-def remote(make_remote, cloud_name):
-    yield make_remote(name="upstream", typ=cloud_name)
+def remote(make_remote):
+    yield make_remote(name="upstream", typ="s3")
 
 
 @pytest.fixture
-def workspace(make_workspace, cloud_name):
-    yield make_workspace(name="workspace", typ=cloud_name)
+def workspace(make_workspace):
+    yield make_workspace(name="workspace", typ="s3")
 
 
 @pytest.fixture
 def stage_md5():
-    raise NotImplementedError
+    return "2aa17f8daa26996b3f7a4cf8888ac9ac"
 
 
 @pytest.fixture
 def is_object_storage():
-    raise NotImplementedError
+    return True
 
 
 @pytest.fixture
 def dir_md5():
-    raise NotImplementedError
+    return "ec602a6ba97b2dd07bd6d2cd89674a60.dir"
 
 
 @pytest.fixture
 def hash_name():
-    raise NotImplementedError
+    return "etag"
 
 
 @pytest.fixture
 def hash_value():
-    raise NotImplementedError
+    return "8c7dd922ad47494fc02c388e12c00eac"
 
 
 @pytest.fixture
 def dir_hash_value(dir_md5):
-    raise NotImplementedError
-
+    return dir_md5
