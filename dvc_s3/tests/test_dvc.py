378b642b2811eded58a2c52b1900f548b1a0bf46
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-05-04 18:06:30 +0200

    test_dvc: Add TestImportS3 and TestAddS3.
    Fix unintentional skips.

--- dvc_s3/tests/test_dvc.py
+++ dvc_s3/tests/test_dvc.py
@@ -19,31 +19,29 @@ def workspace(make_workspace):
     yield make_workspace(name="workspace", typ="s3")
 
 
-@pytest.fixture
-def stage_md5():
-    return "2aa17f8daa26996b3f7a4cf8888ac9ac"
+class TestImportS3(TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        return "2aa17f8daa26996b3f7a4cf8888ac9ac"
+
+    @pytest.fixture
+    def is_object_storage(self):
+        return True
+
+    @pytest.fixture
+    def dir_md5(self):
+        return "ec602a6ba97b2dd07bd6d2cd89674a60.dir"
 
 
-@pytest.fixture
-def is_object_storage():
-    return True
+class TestAddS3(TestAdd):
+    @pytest.fixture
+    def hash_name(self):
+        return "etag"
 
+    @pytest.fixture
+    def hash_value(self):
+        return "8c7dd922ad47494fc02c388e12c00eac"
 
-@pytest.fixture
-def dir_md5():
-    return "ec602a6ba97b2dd07bd6d2cd89674a60.dir"
-
-
-@pytest.fixture
-def hash_name():
-    return "etag"
-
-
-@pytest.fixture
-def hash_value():
-    return "8c7dd922ad47494fc02c388e12c00eac"
-
-
-@pytest.fixture
-def dir_hash_value(dir_md5):
-    return dir_md5
+    @pytest.fixture
+    def dir_hash_value(self):
+        return "ec602a6ba97b2dd07bd6d2cd89674a60.dir"
