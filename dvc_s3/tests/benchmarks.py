03530ea41836af5098c3862dd31a1b2cfc890bbd
Peter Rowlands <peter@pmrowla.com>
2023-05-05 14:16:04 +0900

    tests: use test_sharing_s3 namespace

--- dvc_s3/tests/benchmarks.py
+++ dvc_s3/tests/benchmarks.py
@@ -1,1 +1,3 @@
-from dvc.testing.benchmarks.cli.stories.use_cases.test_sharing import *  # noqa, pylint: disable=wildcard-import,unused-import
+from dvc.testing.benchmarks.cli.stories.use_cases.test_sharing import (  # noqa, pylint: disable=unused-import
+    test_sharing as test_sharing_s3,
+)
