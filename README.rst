c829795f7c20722daec3f3af226c8cae0ef01c6b
Peter Rowlands <peter@pmrowla.com>
2023-05-05 14:08:05 +0900

    tests: use real s3 when DVC_TEST_AWS_REPO_BUCKET is set

--- README.rst
+++ README.rst
@@ -1,3 +1,10 @@
 dvc-s3
+======
 
 s3 plugin for dvc
+
+Tests
+-----
+
+By default tests will be run against moto (via pytest-servers).
+To run against real S3, set ``DVC_TEST_AWS_REPO_BUCKET`` with an AWS bucket name.
--- dvc_s3/tests/fixtures.py
+++ dvc_s3/tests/fixtures.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from .cloud import S3, FakeS3
@@ -5,8 +7,12 @@ from .cloud import S3, FakeS3
 
 @pytest.fixture
 # pylint: disable-next=redefined-outer-name,unused-argument
-def make_s3(tmp_s3_path, s3_server, request):
+def make_s3(request):
     def _make_s3():
+        if os.environ.get("DVC_TEST_AWS_REPO_BUCKET"):
+            return S3(S3.get_url())
+        tmp_s3_path = request.getfixturevalue("tmp_s3_path")
+        s3_server = request.getfixturevalue("s3_server")
         return FakeS3(str(tmp_s3_path).rstrip("/"), endpoint_url=s3_server)
 
     return _make_s3
@@ -24,11 +30,6 @@ def make_s3_version_aware(versioning, tmp_s3_path, s3_server):
 @pytest.fixture
 def s3(make_s3):  # pylint: disable=redefined-outer-name
     yield make_s3()
-
-
-@pytest.fixture
-def real_s3():
-    yield S3(S3.get_url())
 
 
 @pytest.fixture
