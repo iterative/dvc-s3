b7791d40dec88f694f93bd4e8d64766574ca65bd
Peter Rowlands <peter@pmrowla.com>
2023-05-16 16:35:25 +0900

    benchmarks: remove test_modify_data from benchmarks

--- dvc_s3/tests/benchmarks.py
+++ dvc_s3/tests/benchmarks.py
@@ -1,11 +1,5 @@
 # pylint: disable=unused-import
 # noqa
-from dvc.testing.benchmarks.cli.stories.test_modify_data import (  # noqa
-    test_partial_add as test_partial_add_s3,
-)
-from dvc.testing.benchmarks.cli.stories.test_modify_data import (  # noqa
-    test_partial_remove as test_partial_remove_s3,
-)
 from dvc.testing.benchmarks.cli.stories.use_cases.test_sharing import (  # noqa
     test_sharing as test_sharing_s3,
 )
